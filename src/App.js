71cb7804beae17267cc46a9e22d6fc9d21bf0d24
Yousif AlOzairi <yoyo012@icloud.com>
2020-11-06 22:19:54 +0300

    Code formatting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
         </div>
-               <div>
- <p>
-          Here is the <code>Zombie</code> Menu.
-        </p>        </div>
+        <div>
+          <p>
+            Here is the <code>Zombie</code> Menu.
+          </p>
+        </div>
 
-              <div>
-  <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Menu
-        </a>        </div>
-
-
+        <div>
+          <a className="App-link" target="_blank" rel="noopener noreferrer">
+            Menu
+          </a>{" "}
+        </div>
       </header>
     </div>
   );
