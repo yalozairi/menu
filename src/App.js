fbd941a90b056c753c8affce19027f18d977bfcd
Yousif AlOzairi <yoyo012@icloud.com>
2020-11-06 22:18:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </div>
+               <div>
+ <p>
+          Here is the <code>Zombie</code> Menu.
+        </p>        </div>
+
+              <div>
+  <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Menu
+        </a>        </div>
+
+
       </header>
     </div>
   );
